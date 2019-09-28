00851c4051102b828efdb50b23b778e374614564
joao4018 <joao4018@gmail.com>
2019-09-27 21:52:39 -0300

    Movendo widget para pasta widgets

--- lib/screens/name_user.dart
+++ lib/screens/name_user.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/material.dart';
-import 'package:smartmeat/screens/text_section.dart';
-import 'image_banner.dart';
+import 'package:smartmeat/widgets/text_section.dart';
+import 'package:smartmeat/widgets/image_banner.dart';
 import 'package:smartmeat/widgets/buttons.dart';
 
 
@@ -12,14 +12,15 @@ class NameUser extends StatelessWidget {
     return Scaffold(
         resizeToAvoidBottomInset: false,
       body: Column(
+        
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          SizedBox(height: 30,),
           ImageBanner("images/SmartMeat.png"),
           TextSection("Vamos iniciar.", "Por favor,\n digite seu nome\n no campo abaixo!"),
           TextField(
-            decoration: InputDecoration(labelText: "Nome",
-            border: OutlineInputBorder()),
+            decoration: InputDecoration(labelText: "Nome",),
           ),
          Botao('PRONTO!'),
         ],
