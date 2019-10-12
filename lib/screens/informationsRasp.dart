5c770fab0d6db0cbc6472e19787afcc37791ffb7
Davi Alves <davialvb@gmail.com>
2019-10-12 15:43:25 +0000

    Add comunications with socket

--- lib/screens/informationsRasp.dart
+++ lib/screens/informationsRasp.dart
@@ -1,30 +1,81 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:udp/udp.dart';
 
 
-class informationRasp extends StatefulWidget {
+class InformationRasp extends StatefulWidget {
   @override
-  _informationRaspState createState() => _informationRaspState();
+  _InformationRaspState createState() => _InformationRaspState();
 }
 
-class _informationRaspState extends State<informationRasp> {
+class _InformationRaspState extends State<InformationRasp> {
   String message;
+  // var str;
+  
+  informations() async {
+      RawDatagramSocket socket;
+        socket = await RawDatagramSocket.bind("0.0.0.0", 58332);
+        await for (RawSocketEvent ev in socket.asBroadcastStream()) {          
+          if (ev == RawSocketEvent.read) {
+            try {
+              Datagram dg = socket.receive();
+              if (dg != null) {
+                print("${dg.address} ${dg.port} ${dg.data}");
+                socket.send(dg.data, dg.address, dg.port);
+              }
+            } catch (e) {
+              print("#########$e");
+            }
+          }
+        }
 
-  void informations(){
-  RawDatagramSocket.bind(InternetAddress.ANY_IP_V4, 58332).then((RawDatagramSocket socket){
-    print('Datagram socket ready to receive');
-    print('${socket.address.address}:${socket.port}');
-    socket.listen((RawSocketEvent e){
-      Datagram d = socket.receive();
-      if (d == null) return;
-      setState(() {
-          message = new String.fromCharCodes(d.data).trim();
-      });
+  //   var reciever = await UDP.bind(Endpoint.loopback(port: Port(58332)));
+  //   print("$reciever.listen");
 
-        print('Datagram from ${d.address.address}:${d.port}: ${message}');
-      });
-    });
-  }
+  //   await reciever.listen((datagram){
+  //     str = String.fromCharCodes(datagram.data);
+  //     // stdout.write(str);
+  //     print("ola");
+  //   }, Duration(seconds: 20));
+    
+    // RawDatagramSocket socket;
+    // startServer(String host, int port) async {
+    //   socket = await RawDatagramSocket.bind(host, port);
+    //   await for (RawDatagramSocket ev in socket.asBroadcastStream()){
+    //     if (ev == RawDatagramSocket.READ){
+    //       try {
+    //         Datagram dg = socket.receive();
+    //         if(dg != null) {
+    //           print("${dg.address} ${dg.port} $dg.data");
+    //           socket.send(dg.data, dg.address, dg.port);
+    //         }
+    //       } catch (e){
+    //         print("#####$e");
+    //       }
+    //     }
+    //   }
+    // }
+    // RawDatagramSocket.bind(InternetAddress.anyIPv4, 58332).then((RawDatagramSocket socket){
+    //   print('Datagram socket ready to receive');
+    //   print('${socket.address.address}:${socket.port}');
+    //   socket.listen((RawSocketEvent e){
+    //     Datagram d = socket.receive();
+    //     print("${d.data}");
+
+    //     //if (d == null) return;
+    //     setState(() {
+    //         message = new String.fromCharCodes(d.data).trim();
+    //     });
+
+    //     print('Datagram from ${d.address.address}:${d.port}: $message');
+    //     });
+    //   });  
+}
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +105,7 @@ class _informationRaspState extends State<informationRasp> {
                                 ),
                                 SizedBox(width: 40.0),
                                 Text(
-                                  'Tirar Foto',
+                                  'Testar Socket',
                                   style: TextStyle(
                                       color: Colors.black, fontSize: 25),
                                 ),
