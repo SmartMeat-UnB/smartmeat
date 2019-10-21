312e1aaddd555365b9130d4c7c19101a69a9f012
joao4018 <joao4018@gmail.com>
2019-10-21 20:58:58 -0300

    finalizando tratamento json para objeto

--- lib/screens/testt.dart
+++ lib/screens/testt.dart
@@ -58,7 +58,7 @@ class Blabla extends StatelessWidget {
           '{"recipes": [{"ingrs": ["zucchini","oil","salt","squash","pepper","eggplant"],"recipe": ["Preheat grill to medium-high.","Brush eggplant slices with oil and season with salt and pepper.","Grill, turning once, until tender and lightly charred, about 10 minutes.","Transfer to a platter and let cool.","Cut into 1/2-inch slices.","Serve with grilled zucchini."],"title": "Grilled eggplant and zucchini"},{"ingrs": ["zucchini","oil","salt","squash","pepper","eggplant"],"recipe": ["Slice the squash and slice into rounds, then brush with olive oil.","Season with salt and pepper.","Grill over high heat, turning once to char all sides.","Sprinkle with fresh grated cheese and serve."],"title": "Eggplant with zucchini (aubergine)"}]}';
       var parsedJson = json.decode(jsonData);
       var user = Receita.fromJson(parsedJson);
-      print('${user.recipes[1]}');
+      print('${user.recipes[1].ingrs}');
     }
 
     Widget results() {
