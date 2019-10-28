d71e1916c3908a296d3aa29f6959260553527077
joao4018 <joao4018@gmail.com>
2019-10-28 16:54:28 -0300

    Finalizando Notificacoes

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -163,6 +163,66 @@ class _SettingsState extends State<Settings> {
                     ],
                   ),
                 ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15),
+                  decoration: BoxDecoration(
+                      border: Border(
+                    bottom: BorderSide(color: Colors.black),
+                  )),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(children: <Widget>[
+                        Icon(
+                          Icons.notifications_active,
+                          size: 40,
+                          // color: Colors.black,
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text("Wi-Fi", style: TextStyle(fontSize: 18)),
+                        SizedBox(
+                          width: 165,
+                        ),
+                        Switch(
+                          onChanged: (bool value) {
+                            setState(() {
+                              value = !_state;
+                            });
+                          },
+                          activeColor: Colors.green,
+                          value: _state,
+                        ),
+                      ]),
+                      TextField(
+                        onChanged: (text) {
+                          _doSomething(text);
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.timer,
+                            size: 40,
+                            color: Colors.black,
+                          ),
+                          suffixIcon: Icon(
+                            Icons.edit,
+                            size: 40,
+                            color: Colors.black,
+                          ),
+                          border: InputBorder.none,
+                          labelText: " 15 min",
+                          labelStyle: TextStyle(
+                            height: 0,
+                            fontSize: 20,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
