32356a3a87656a07e30e90de07f5d142c1c6095b
joao4018 <joao4018@gmail.com>
2019-11-06 22:27:59 -0300

    Refazendo configuracao do nome

--- lib/screens/intro.dart
+++ lib/screens/intro.dart
@@ -1,3 +1,4 @@
+import 'package:SmartMeat/screens/name_user.dart';
 import 'package:SmartMeat/screens/tutorialWifi.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
@@ -6,63 +7,71 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: DefaultTabController(
-      length: choices.length,
-      child: Builder(
-        builder: (BuildContext context) => Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: IconTheme(
-                  data: IconThemeData(
-                    size: 128.0,
-                    color: Theme.of(context).accentColor,
+      body: DefaultTabController(
+        length: choices.length,
+        child: Builder(
+          builder: (BuildContext context) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: IconTheme(
+                    data: IconThemeData(
+                      size: 128.0,
+                      color: Theme.of(context).accentColor,
+                    ),
+                    child: TabBarView(
+                      children: choices.map((Choice choice) {
+                        return Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: ChoiceCard(choice: choice),
+                        );
+                      }).toList(),
+                    ),
                   ),
-                  child: TabBarView(children: choices.map((Choice choice) {
-                          return Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: ChoiceCard(choice: choice),
-                          );
-                        }).toList(),),
                 ),
-              ),
-              Container(
-                alignment: Alignment(0.0,0.9),
-                padding: EdgeInsets.all(20.0),
-                child: ButtonTheme(
-                  minWidth: 100.0,
-                  height: 60.0,                  
-                    child:RaisedButton(
-                      child: Text('INICIAR',
-                                  style: TextStyle(
-                                            fontSize: 40.0,
-                                            color: Colors.white,
-                                          ),
-                                ),
-                      color: Colors.black,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-                      onPressed: () {
-                        final TabController controller =
-                            DefaultTabController.of(context);
-                        if (!controller.indexIsChanging) {
-                          controller.animateTo(choices.length - 1);
-                        }
-                        Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) => TutorialWifi()));
-                      },
-                  )
+                Container(
+                  alignment: Alignment(0.0, 0.9),
+                  padding: EdgeInsets.all(20.0),
+                  child: ButtonTheme(
+                      minWidth: 100.0,
+                      height: 60.0,
+                      child: RaisedButton(
+                        child: Text(
+                          'INICIAR',
+                          style: TextStyle(
+                            fontSize: 40.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        color: Colors.black,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)),
+                        onPressed: () {
+                          final TabController controller =
+                              DefaultTabController.of(context);
+                          if (!controller.indexIsChanging) {
+                            controller.animateTo(choices.length - 1);
+                          }
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (context) => NameUser()));
+                        },
+                      )),
                 ),
-              ),
-              TabPageSelector(color: Colors.white,selectedColor: Colors.red,)
-            ],
+                TabPageSelector(
+                  color: Colors.white,
+                  selectedColor: Colors.red,
+                )
+              ],
+            ),
           ),
         ),
       ),
-    ),
     );
   }
 }
+
 class Choice {
   const Choice({this.text, this.imageFile});
 
@@ -87,31 +96,32 @@ class ChoiceCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return 
-      Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 50,),
-            Image.asset(
-                          choice.imageFile,
-                          fit: BoxFit.cover,
-                          height: 190.0,
-                        ),
-            SizedBox(height: 50,),
-
-            Expanded( 
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            height: 50,
+          ),
+          Image.asset(
+            choice.imageFile,
+            fit: BoxFit.cover,
+            height: 190.0,
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Expanded(
               child: AutoSizeText(
-                choice.text,
-                presetFontSizes: [40.0, 20.0, 14.0],
-                maxLines: 4,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-              )
-            ),
-          ],
-        ),
-      );
+            choice.text,
+            presetFontSizes: [40.0, 20.0, 14.0],
+            maxLines: 4,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )),
+        ],
+      ),
+    );
   }
 }
