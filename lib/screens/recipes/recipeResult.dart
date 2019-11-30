d4a405179c8d5bf42fabf94bf9f3886be12f3770
Gabriel Ziegler <gabrielziegler3@gmail.com>
2019-11-30 15:59:30 -0300

    Adjust responsiviness and padding in recipes
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

--- lib/screens/recipes/recipeResult.dart
+++ lib/screens/recipes/recipeResult.dart
@@ -11,11 +11,13 @@ class RecipeResult extends StatelessWidget {
   final Recipes receita;
   final int contador;
   RecipeResult(this.imageFile, this.receita, this.contador);
+
   @override
   Widget build(BuildContext context) {
+    final _width = MediaQuery.of(context).size.width;
+    final _height = MediaQuery.of(context).size.height;
     String title = this.receita.recipes[contador].title;
     List<String> ingrs = this.receita.recipes[contador].ingrs;
-    //final ingredientes = ingrs.reduce((value, element) => value + '\n' + '-' + ' ' + element);
     List<String> recipe = this.receita.recipes[contador].recipe;
 
     Widget buildIngrs() {
@@ -24,8 +26,8 @@ class RecipeResult extends StatelessWidget {
         itemCount: ingrs.length,
         itemBuilder: (BuildContext context, int index) {
           String ingredientes = ingrs[index];
-            return Text(" - $ingredientes" + ",", style: TextStyle(fontSize: 15), textAlign: TextAlign.left,);
-
+          return Text(" - $ingredientes" + ",",
+              style: TextStyle(fontSize: 15), textAlign: TextAlign.left);
         },
       );
     }
@@ -36,7 +38,8 @@ class RecipeResult extends StatelessWidget {
         itemCount: recipe.length,
         itemBuilder: (BuildContext context, int index) {
           String recipes = recipe[index];
-          return Text(" - $recipes", style: TextStyle(fontSize: 15), textAlign: TextAlign.justify);
+          return Text(" - $recipes",
+              style: TextStyle(fontSize: 15), textAlign: TextAlign.justify);
         },
       );
     }
@@ -44,92 +47,82 @@ class RecipeResult extends StatelessWidget {
     return Scaffold(
         bottomNavigationBar: BottomApp(),
         body: SingleChildScrollView(
-            child: Container(
-          padding: EdgeInsets.symmetric(vertical: 7.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                child: Column(children: <Widget>[
-                  Center(
-                    // child: Text(
-                    //   "Receita ${contador + 1}",
-                    //   style:
-                    //       TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-                    //   textAlign: TextAlign.center,
-                    // ),
-                  ),
-                  SizedBox(
-                    height: 7,
-                  ),
-                  Card(
-                      child: Container(
-                    width: 120,
-                    height: 120,
-                    child: Image.file(
-                      imageFile,
-                      fit: BoxFit.cover,
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 7.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Column(children: <Widget>[
+                    Center(),
+                    SizedBox(
+                      height: _height * 0.01,
                     ),
-                  )),
-                  SizedBox(width: 50.0),
-                  Text(
-                    '$title',
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    Card(
+                        child: Container(
+                          height: 120,
+                          width: 120,
+                          child: Image.file(
+                            imageFile,
+                            fit: BoxFit.cover,
+                          ),
+                      )
+                    ),
+                  ]),
+                ),
+                SizedBox(height: _height * 0.02),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: _width * 0.05),
+                  child: Column(
+                    children: <Widget>[
+                      Text("Ingredientes:",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 17),
+                          textAlign: TextAlign.left),
+                    ],
                   ),
-                ]),
-              ),
-              SizedBox(height: 18.0),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 7.0),
-                child: Column(
-                  children: <Widget>[
-                    Text("Ingredientes:",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 17),
-                        textAlign: TextAlign.left),
-                  ],
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 7.0),
-                child: Column(
-                  children: <Widget>[
-                    buildIngrs(),
-                  ],
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: _width * 0.05),
+                  child: Column(
+                    children: <Widget>[
+                      buildIngrs(),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 22.0),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 7.0),
-                child: Column(
-                  children: <Widget>[
-                    AutoSizeText("Modo de preparo:",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 17),
-                        textAlign: TextAlign.left),
-                  ],
+                SizedBox(height: _height * 0.02),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: _width * 0.05),
+                  child: Column(
+                    children: <Widget>[
+                      AutoSizeText("Modo de preparo:",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 17),
+                          textAlign: TextAlign.left),
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 7.0),
-                child: Column(
-                  children: <Widget>[
-                    buildRecipes(),
-                  ],
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: _width * 0.05),
+                  child: Column(
+                    children: <Widget>[
+                      buildRecipes(),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
         ),
         appBar: AppBar(
           backgroundColor: Colors.red[900],
-          title: AutoSizeText("$title",
-          style: TextStyle(
-            fontSize: 35.0,
-            fontFamily: 'Pacifico',
-          ),
+          title: AutoSizeText(
+            "$title",
+            style: TextStyle(
+              fontSize: 35.0,
+              fontFamily: 'Pacifico',
+            ),
           ),
           leading: IconButton(
             icon: Icon(
